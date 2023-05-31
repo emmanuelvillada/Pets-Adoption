272d26fda888bcdee28b258ee9408ee072390cdc
EMMANUEL VILLADA SUAREZ <emmanuelvillada310016@correo.itm.edu.co>
2023-05-30 23:12:40 -0500

    new  controller

---
+++ Pets-Adpotion/Pets-Adpotion/Controllers/AdopterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pets_Adpotion.Controllers
+{
+    public class AdopterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
